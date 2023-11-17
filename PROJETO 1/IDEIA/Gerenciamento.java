59b36603f4da8b622c2fd3041b3e676397179ada
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2023-11-17 18:34:32 -0300

    Create Gerenciamento.java

---
+++ PROJETO 1/IDEIA/Gerenciamento.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Gerenciamento {
+	
+	    private String nome;
+	    private int senha;
+
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+
+	    public int getSenha() {
+	        return senha;
+	    }
+
+	    public void setSenha(int senha) {
+	        this.senha = senha;
+	    }
+	}
+
+
