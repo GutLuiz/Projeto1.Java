144f65d3ad3b5374278ea37e481a4ee028d2487e
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2023-11-23 19:39:30 -0300

    Update Aposta.java

--- PROJETO 1/CODIGOS/Aposta.java
+++ PROJETO 1/CODIGOS/Aposta.java
@@ -1,1 +1,23 @@
 
+package entities;
+
+public class Aposta {
+	 
+	    private double valor;
+	    
+	    public Aposta(double valor) {
+	    	this.valor = valor;
+	    }
+	   
+		public double getValor() {
+			return valor;
+		}
+		
+		public void setValor(double valor) {
+			this.valor = valor;
+		}
+
+	    
+	}
+
+
