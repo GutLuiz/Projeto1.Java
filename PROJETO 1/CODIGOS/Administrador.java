e03591e253da6becd58743936c277d96c505fa09
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2023-11-23 19:39:10 -0300

    Update Administrador.java

--- PROJETO 1/CODIGOS/Administrador.java
+++ PROJETO 1/CODIGOS/Administrador.java
@@ -1,1 +1,63 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Administrador extends Gerenciamento {
+	SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy");
+		
+		private List<Evento> eventos = new ArrayList<>();
+		
+		public Administrador() {
+		}
+
+		public Administrador(String nome, int senha) {
+			super.setNome(nome);
+			super.setSenha(senha);
+		}
+
+
+		public List<Evento> getEvento() {
+			return eventos;
+		}
+		public void adicionarEvento(Evento event) {
+		     eventos.add(event);
+		}
+
+		 public void removerEvento(Evento event) {
+		     eventos.remove(event);
+		}
+		 
+		public Evento obterEventoPorId(int id) {
+			for(Evento evento : eventos) {
+				if(evento.getId() == id) {
+					return evento;
+				}
+			}
+			return null;
+		}
+
+		public String toString() {
+			StringBuilder event = new StringBuilder();
+			event.append("Evento esportivo:  \n");
+			for(Evento jogo : eventos) {
+				event.append("PARTIDA: " + jogo.getEquipe1().getNome() + "(" + jogo.getEquipe1().getOdd() + ") ");
+				event.append("x ");
+				event.append(jogo.getEquipe2().getNome() + "(" + jogo.getEquipe2().getOdd() + ")\n");
+				event.append("DATA DA PARTIDA: " + sdf.format(jogo.getData()) + "\n");
+				event.append("LOCAL DA PARTIDA : " + jogo.getLocal());
+			}
+			return event.toString();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
 
